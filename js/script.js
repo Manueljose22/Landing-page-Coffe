0cc8f3cbfe801b210d80e01f074d2ab98ba537a9
Manuel Leonel <manuelleonel73@gmail.com>
2023-04-02 14:15:34 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -13,6 +13,35 @@ let btnMenu = document.querySelector('#menu-icon')
 
 btnMenu.addEventListener('click', () =>{
     btnMenu.classList.toggle('bx-x')
-    navMenu.classList.toggle('open')
-    
+    navMenu.classList.toggle('open')  
 })
+
+let containerArticle = document.querySelectorAll('.row')
+function SliderClients(indice) {
+   let n = indice
+
+   let mover =  100
+   let next = mover
+   let preview = -mover
+
+   if (indice == +1) {
+    containerArticle[n].scrollBy(next, 0)
+
+   }
+   if (indice == -1) {
+    containerArticle[n].scrollBy(preview, 0)
+   }
+
+   setTimeout(() => {
+    for (let i = 0; i <= containerArticle.length; i++) {
+        containerArticle[i]
+        
+    }
+   }, 3000);
+}
+
+SliderClients(-1)
+
+
+
+
